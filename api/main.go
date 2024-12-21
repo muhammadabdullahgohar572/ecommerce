a914c9a0d9cfc406261d8133ea890fce32ef35af
Abdullah <muhammadabdullahgohar572@gmail.com>
2024-12-21 15:28:44 +0500

    first commit

--- api/main.go
+++ api/main.go
@@ -12,41 +12,42 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-
-var(
-	mongoURI   ="mongodb+srv://muhammadabdullahgohar572:ilove1382005@cluster0.kxsr5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-	client    *mongo.Client
+var (
+	mongoURI       = "mongodb+srv://muhammadabdullahgohar572:ilove1382005@cluster0.kxsr5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+	client         *mongo.Client
 	usersCollection *mongo.Collection
 )
 
-func initMongo() {
+func init() {
+	// Initialize MongoDB connection once
 	var err error
-	client, err = mongo.Connect(context.TODO(),options.Client().ApplyURI(mongoURI))
-    
-	if err !=nil {
+	client, err = mongo.Connect(context.TODO(), options.Client().ApplyURI(mongoURI))
+	if err != nil {
 		log.Fatal("Error connecting to MongoDB:", err)
 	}
-	usersCollection =client.Database("test").Collection("users")
+	usersCollection = client.Database("test").Collection("users")
 	log.Println("Connected to MongoDB")
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	initMongo()
-	router := mux.NewRouter()
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "hello go from vercel !!!!",
+	})
+}
 
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(map[string]string{
-			"message": "hello go from vercel !!!!",
-		})
-	}).Methods("GET")
-	
-	corsHandler :=cors.New(cors.Options{
+func handler(w http.ResponseWriter, r *http.Request) {
+	router := mux.NewRouter()
+	router.HandleFunc("/", helloHandler).Methods("GET")
+
+	// Apply CORS middleware
+	corsHandler := cors.New(cors.Options{
 		AllowedOrigins:   []string{"*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
 		AllowedHeaders:   []string{"Authorization", "Content-Type"},
 		AllowCredentials: true,
 	}).Handler(router)
+
+	// Serve the request
 	corsHandler.ServeHTTP(w, r)
 }
-
